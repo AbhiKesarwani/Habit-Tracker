8e8a08d2ce73446f3c213c1beff2540d689e60a7
AbhiKesarwani <163742310+AbhiKesarwani@users.noreply.github.com>
2024-07-03 13:33:59 +0530

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-#pixela api :- https://docs.pixe.la/
-#pixela website :- https://pixe.la/
-
 import requests
-import datetime
 import secret_files
+import datetime
+
+#You can make your own username,token,graph_id and pixela endpoint by just login on Pixela API.
 
 USERNAME = secret_files.s_USERNAME
 TOKEN = secret_files.s_TOKEN
@@ -18,7 +17,7 @@ parameter = {
     "notMinor": "yes",
 }
 
-#response = requests.post(url=pixela_endpoint, json=parameter)
+#response = requests.post(url=pixela_endpoint, json=parameter)    #run only once as this will create a user
 #print(response.text)
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
@@ -34,33 +33,23 @@ graph_parameter = {
 graph_headers = {
     "X-USER-TOKEN": TOKEN
 }
-#response2 = requests.post(url=graph_endpoint, json=graph_parameter, headers=graph_headers)
+#response2 = requests.post(url=graph_endpoint, json=graph_parameter, headers=graph_headers)       #Create graph ad run it only once
 #print(response2.text)
 
 value_graph_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
 
 today = datetime.datetime.now()
+#print(type(today))
 
 value_graph_parameters = {
-    "date": today.strftime("%Y%m%d"),
+    "date": today.strftime('%Y%m%d'),
     "quantity": input("How many min's did you coded today?: "),
 }
 
+#To update the value on graph
 value_graph = requests.post(url=value_graph_endpoint, json=value_graph_parameters, headers=graph_headers)
 print(value_graph.text)
 
-'''
-update_endpoint = f"{value_graph_endpoint}/{today.strftime('%Y%m%d')}"
-updated_parameter = {
-    "quantity": "40",
-}
 
-#updated_response = requests.put(url=update_endpoint, json=updated_parameter, headers=graph_headers)
-#print(updated_response.text)
-
-delete_endpoint = f"{value_graph_endpoint}/{today.strftime('%Y%m%d')}"
-delete_response = requests.delete(url=delete_endpoint, headers=graph_headers)
-print(delete_response.text)
-'''
-
-#Check your graph here "https://pixe.la/v1/users/abhikesarwani38/graphs/myfirstgraph1.html"
+#Check my graph here "https://pixe.la/v1/users/abhikesarwani38/graphs/myfirstgraph1.html"
+#You will also get a graph link which you can search on any browser and see your habits updates
